069a0f51c08133c803c0d4286ff8183c66882c50
IvanL2 <in.ly@outlook.com>
2025-09-25 14:30:43 +0100

    Force 4 spaces tab

--- init.lua
+++ init.lua
@@ -6,6 +6,10 @@ vim.g.mapleader = ","
 -- other vim config
 vim.o.expandtab = true
 vim.bo.expandtab = true
+vim.o.softtabstop = 4
+vim.bo.softtabstop = 4
+vim.o.tabstop = 4
+vim.bo.tabstop = 4
 vim.o.shiftwidth = 4
 vim.bo.shiftwidth = 4
 
