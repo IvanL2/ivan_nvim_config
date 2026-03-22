1b7c8294a4805071410cd73a03d6dacbfb7b429c
IvanL2 <48653968+IvanL2@users.noreply.github.com>
2026-03-22 15:13:16 +0000

    Add treesitter start autocmd

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,5 +2,15 @@ return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   branch = "main",
-  build = ":TSUpdate"
+  build = ":TSUpdate",
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+        if (vim.treesitter.language.add(lang)) then
+          vim.treesitter.start(args.buf, lang)
+        end
+      end
+    })
+  end
 }
